a2a6a1d9ee285db69c7dc738f71bbeb3d1ef7aed
teklu abayneh <tekluabayneh@gmail.com>
2025-10-09 15:47:23 +0300

    feat: add tailwindcss

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+function App() {
+
+  return (
+		<>
+  <h1 >dashboard</h1>
+<h1 className="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  </>
+ )
+
+}
+
+export default App
