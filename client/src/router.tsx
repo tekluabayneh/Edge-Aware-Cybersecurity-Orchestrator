eef5755984c301c51f57b1bfb69d7adaab8f8e9a
teklu abayneh <tekluabayneh@gmail.com>
2025-10-13 13:49:23 +0300

    refactor: update dashboard, profile, and router after component removal

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -2,12 +2,16 @@ import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loading from "./components/loading";
 const Dashboard = lazy(() => import("./pages/Dashboard/dashbaord"));
+const Alerts = lazy(() => import("./pages/Alert/Alert"))
+const Profiles = lazy(() => import("./pages/Profile/Profile"))
 
 const Routers = () => {
   return (
     <Suspense fallback={<Loading />}>
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/Profile" element={<Profiles/>} />
+        <Route path="/Alerts" element={<Alerts/>} />
       </Routes>
     </Suspense>
   );
