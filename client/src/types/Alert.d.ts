168f642c3a34ce42216b69e5240a0c5115bdc920
teklu abayneh <tekluabayneh@gmail.com>
2025-10-14 16:04:41 +0300

    Add types directory

---
+++ client/src/types/Alert.d.ts
@@ -0,0 +1,24 @@
+export type AlertType = {
+    id: number;
+    title: string;
+    description: string;
+    severity: string;
+    status: string;
+    detected_at: string;
+    source_ip: string;
+    target: string;
+};
+
+export type userType = {
+    email: string;
+    full_name: string;
+    created_date: string;
+};
+
+type MetricCardProps = {
+    title: string;
+    value: string | number;
+    change: string;
+    trend: "up" | "down";
+    icon: React.ComponentType;
+};
