9f31c30cf3d627fe02386dff362364358c70c3b1
teklu abayneh <tekluabayneh@gmail.com>
2025-10-17 23:41:16 +0300

    Update client router configuration

--- client/src/router.tsx
+++ client/src/router.tsx
@@ -1,19 +1,29 @@
 import React, { Suspense, lazy } from "react";
 import { Routes, Route } from "react-router-dom";
 import Loading from "./components/loading";
+
+import Footer from "./components/footer/footer";
 const Dashboard = lazy(() => import("./pages/Dashboard/dashbaord"));
-const Alerts = lazy(() => import("./pages/Alert/Alert"))
-const Profiles = lazy(() => import("./pages/Profile/Profile"))
+const Alerts = lazy(() => import("./pages/Alert/Alert"));
+const Profiles = lazy(() => import("./pages/Profile/Profile"));
+const Landing = lazy(() => import("./pages/Landing/landing"));
+const Auth = lazy(() => import("./pages/Auth/Auth"));
 
 const Routers = () => {
   return (
-    <Suspense fallback={<Loading />}>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/Profile" element={<Profiles/>} />
-        <Route path="/Alerts" element={<Alerts/>} />
-      </Routes>
-    </Suspense>
+    <>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/Profile" element={<Profiles />} />
+          <Route path="/Alerts" element={<Alerts />} />
+        </Routes>
+      </Suspense>
+      {/* Footer */}
+      <Footer />
+    </>
   );
 };
 export default Routers;
