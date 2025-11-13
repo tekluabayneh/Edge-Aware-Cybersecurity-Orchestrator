101fccd1778364bb428ac1255eb7794ea8f9c0dd
Teklu <tekluabayneh@gmail.com>
2025-11-14 01:12:12 +0400

    update login handler logic and improve error handling

--- backend/internal/auth/login.go
+++ backend/internal/auth/login.go
@@ -2,7 +2,10 @@ package handler
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+
+	"github.com/edge-aware-cyberSecurity/internal/middleware"
 )
 
 type AuthLoginHandlerType struct{}
@@ -13,7 +16,18 @@ type LoginStructure struct {
 }
 
 func (h *AuthLoginHandlerType) Login(w http.ResponseWriter, r *http.Request) {
+	formData, ok := r.Context().Value(middleware.LoginFromDataKey).(middleware.FormType)
+	if !ok {
+		log.Fatal("error loading form data")
+	}
 
-	 
+	fmt.Println(formData)
+	// TODO: Check if the user exists in the database by email
+	// TODO: Compare the provided password with the hashed password
+	// TODO: Handle incorrect credentials safely (do not reveal which field failed)
+	// TODO: Generate a JWT or session token on successful login
+	// TODO: Optionally log login attempts or add rate limiting
+	// TODO: Optionally add multi-factor authentication (MFA)
+
 	fmt.Println("test work")
 }
