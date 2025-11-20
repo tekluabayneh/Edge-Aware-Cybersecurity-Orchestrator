0d4f682c27a03fc857b5e46994ad6e887e8a3976
Teklu <tekluabayneh@gmail.com>
2025-11-20 15:41:14 +0400

    Add agent queries

---
+++ backend/db/queries/agent.sql
@@ -0,0 +1,20 @@
+-- name: GetAllAgent:one
+SELECT * FROM agents LIMIT =50;
+
+-- name: GetAgentById:one
+SELECT * from agents WHERE id = $1;
+
+-- name: CreateAgent:exec
+INSERT INTO agents(user_id, machine_id, agent_version, os, status, last_seen) VALUES ($1, $2, $3, $4, $5, $6);
+
+-- name: UpdateSingleAgent:exec
+UPDATE agents
+SET
+    user_id       = COALESCE($1, user_id),
+    machine_id    = COALESCE($2, machine_id),
+    agent_version = COALESCE($3, agent_version),
+    os            = COALESCE($4, os),
+    status        = COALESCE($5, status),
+    last_seen     = COALESCE($6, last_seen)
+WHERE id = $7;
+
