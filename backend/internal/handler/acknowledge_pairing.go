c75078255cc7d64ab10124ccecd48423d7a24c2c
Teklu <tekluabayneh@gmail.com>
2025-11-24 16:59:27 +0400

    Update acknowledge_pairing handler logic

--- backend/internal/handler/acknowledge_pairing.go
+++ backend/internal/handler/acknowledge_pairing.go
@@ -1,20 +1,70 @@
 package handler
 
 import (
+	"encoding/json"
 	"net/http"
-	"time"
+
+	db "github.com/edge-aware-cyberSecurity/db/sqlc"
+	"github.com/edge-aware-cyberSecurity/internal/utils"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Agent struct {
-	ID           string    `json:"id"`
-	UserID       string    `json:"user_id"`
-	MachineID    string    `json:"machine_id"`
-	AgentVersion string    `json:"agent_version"`
-	OS           string    `json:"os"`
-	Status       string    `json:"status"`
-	LastSeen     time.Time `json:"last_seen"`
+	UserID       int64              `json:"user_id"`
+	AgentToken   string             `json:"agent_token"`
+	AgentId      string             `json:"agent_id"`
+	MachineID    string             `json:"machine_id"`
+	AgentVersion pgtype.Text        `json:"agent_version"`
+	Os           pgtype.Text        `json:"os"`
+	Status       pgtype.Text        `json:"status"`
+	LastSeen     pgtype.Timestamptz `json:"last_seen"`
 }
 
 func (h *DevicePairingType) AcknowledgePairing(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	var AgentValue Agent
+	err := json.NewDecoder(r.Body).Decode(&AgentValue)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusBadRequest, map[string]any{
+			"message": "invalid request",
+			"dl":      err.Error(),
+			"ack":     false,
+		})
+		return
+	}
+	if !utils.RequiredFieldsSet(utils.Agent(AgentValue)) {
+		utils.WriteJSON(w, http.StatusBadRequest, map[string]any{
+			"message": "missing required info",
+			"ack":     false,
+		})
+		return
+	}
+
+	AgentInfo := db.CreateAgentParams{
+		UserID:       AgentValue.UserID,
+		AgentToken:   AgentValue.AgentToken,
+		AgentID:      AgentValue.AgentId,
+		MachineID:    AgentValue.MachineID,
+		AgentVersion: AgentValue.AgentVersion,
+		Os:           AgentValue.Os,
+		Status:       AgentValue.Status,
+		LastSeen:     AgentValue.LastSeen,
+	}
+
+	err = h.DB.CreateAgent(ctx, AgentInfo)
+
+	if err != nil {
+		utils.WriteJSON(w, http.StatusInternalServerError, map[string]any{
+			"message": "internal server error",
+			"dl":      err.Error(),
+			"ack":     false,
+		})
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, map[string]any{
+		"message": "agent info created successfully",
+		"ack":     true,
+	})
 
 }
