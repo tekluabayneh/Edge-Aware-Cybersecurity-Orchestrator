3dc63e0833aa0cd3af38fbb83efe559e14723cc5
Teklu <tekluabayneh@gmail.com>
2025-11-24 16:59:26 +0400

    Update agent SQL queries

--- backend/db/queries/agent.sql
+++ backend/db/queries/agent.sql
@@ -5,7 +5,7 @@ SELECT * FROM agents LIMIT  50;
 SELECT * from agents WHERE id = $1;
 
 -- name: CreateAgent :exec
-INSERT INTO agents(user_id, machine_id, agent_version, os, status, last_seen) VALUES ($1, $2, $3, $4, $5, $6);
+INSERT INTO agents(user_id, agent_id, agent_token, machine_id, agent_version, os, status, last_seen) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);
 
 -- name: GetUserDeviceCount :one
 SELECT COUNT(*) from agents WHERE user_id = $1;
@@ -15,10 +15,11 @@ SELECT COUNT(*) from agents WHERE user_id = $1;
 UPDATE agents
 SET
     user_id       = COALESCE($1, user_id),
-    machine_id    = COALESCE($2, machine_id),
-    agent_version = COALESCE($3, agent_version),
-    os            = COALESCE($4, os),
-    status        = COALESCE($5, status),
-    last_seen     = COALESCE($6, last_seen)
-WHERE id = $7;
-
+    agent_id = COALESCE($2, agent_id),
+    agent_token = COALESCE($3, agent_token),
+    machine_id    = COALESCE($3, machine_id),
+    agent_version = COALESCE($4, agent_version),
+    os            = COALESCE($5, os),
+    status        = COALESCE($6, status),
+    last_seen     = COALESCE($7, last_seen)
+WHERE id = $8;
