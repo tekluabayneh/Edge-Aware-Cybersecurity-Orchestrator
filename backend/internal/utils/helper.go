35d069f9b38defa5ab5966c07e67e35eb13a095e
Teklu <tekluabayneh@gmail.com>
2025-11-26 00:09:42 +0900

    Update helper utilities

--- backend/internal/utils/helper.go
+++ backend/internal/utils/helper.go
@@ -1,13 +1,15 @@
 package utils
 
 import (
+	"net/http"
+
 	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type Agent struct {
 	UserID       int64              `json:"user_id"`
 	AgentToken   string             `json:"agent_token"`
-	AgentId      string             `json:"agent_id"`
+	AgentId      int64              `json:"agent_id"`
 	MachineID    string             `json:"machine_id"`
 	AgentVersion pgtype.Text        `json:"agent_version"`
 	Os           pgtype.Text        `json:"os"`
@@ -24,4 +26,13 @@ func RequiredFieldsSet(a Agent) bool {
 	}
 	return true
 
+}
+
+func CheckError(w http.ResponseWriter, err error, status int, Msg string) {
+	if err != nil {
+		WriteJSON(w, status, map[string]string{
+			"message": Msg,
+		})
+		return
+	}
 }
