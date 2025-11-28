1ee444a41098b5b29abd08227b46e34998db9a2e
Teklu <tekluabayneh@gmail.com>
2025-11-29 00:20:43 +0900

    Update Alerts handler

--- backend/internal/handler/Alerts.go
+++ backend/internal/handler/Alerts.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
+	"reflect"
 	"strconv"
 
 	db "github.com/edge-aware-cyberSecurity/db/sqlc"
@@ -29,75 +32,140 @@ type AlertType struct {
 // GET /api/alerts
 func (h *AlertType) Alerts(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+
 	email := r.Context().Value("email").(string)
 	user, err := h.DB.GetUserByEmail(ctx, email)
-	fmt.Println(user)
 	if utils.CheckError(w, err, http.StatusBadRequest, "user not found") {
 		return
 	}
+
+	// Get all alerts for the user
 	alerts, err := h.DB.GetAllAlert(ctx, int64(user.ID))
 	if utils.CheckError(w, err, http.StatusBadRequest, "agent not found") {
 		return
 	}
 
-	var rawJson []map[string]interface{}
-	err = json.Unmarshal(alerts.RawPayload, &rawJson)
-	if utils.CheckError(w, err, http.StatusBadRequest, "agent not found") {
-		return
+	var allAlerts []map[string]any
+	for _, alert := range alerts {
+		fields := []string{"Network", "Performance", "Security", "RawPayload"}
+		unmarshaledData := make(map[string][]map[string]any)
+
+		val := reflect.ValueOf(alert)
+		for _, field := range fields {
+			f := val.FieldByName(field)
+			if !f.IsValid() {
+				continue
+			}
+
+			fieldBytes, ok := f.Interface().([]byte)
+			if !ok {
+				continue
+			}
+
+			var tmp []map[string]any
+			err := json.Unmarshal(fieldBytes, &tmp)
+			if utils.CheckError(w, err, http.StatusBadRequest, "agent not found") {
+				return
+			}
+
+			unmarshaledData[field] = tmp
+		}
+
+		alertMap := map[string]any{
+			"network":     unmarshaledData["Network"],
+			"performance": unmarshaledData["Performance"],
+			"security":    unmarshaledData["Security"],
+			"raw":         unmarshaledData["RawPayload"],
+			"alertType":   alert.AlertType,
+			"agent_id":    alert.AgentID,
+			"message":     alert.Message,
+			"summery":     alert.Summary,
+			"agent_token": alert.AgentToken,
+			"risk_level":  alert.RiskLevel,
+			"status":      alert.Status,
+		}
+		allAlerts = append(allAlerts, alertMap)
 	}
 
-	var SecurityJson []map[string]interface{}
-	err = json.Unmarshal(alerts.Security, &SecurityJson)
-	if utils.CheckError(w, err, http.StatusBadRequest, "agent not found") {
-		return
-	}
-
-	var PerformanceJson []map[string]interface{}
-	err = json.Unmarshal(alerts.Performance, &PerformanceJson)
-	if utils.CheckError(w, err, http.StatusBadRequest, "agent not found") {
-		return
-	}
-
-	var networkJson []map[string]interface{}
-	err = json.Unmarshal(alerts.Network, &networkJson)
-	if utils.CheckError(w, err, http.StatusBadRequest, "agent not found") {
-		return
-	}
-
-	/* AlertToSend = map[string]interface{}{
-		"network":     networkJson,
-		"performance": PerformanceJson,
-		"security":    SecurityJson,
-		"raw":         rawJson,
-		"alertType":   alerts.AlertType,
-		"agent_id":    alerts.agent_id,
-		"message":     alerts.Message,
-		"summery":     alerts.Security,
-		"agent_token": alerts.agent_token,
-		"risk_level":  alerts.RiskLevel,
-		"status":      alerts.Status,
-	}
-	fmt.Println(AlertToSend) */
-
-	// Encode the actual structured data
-	// json.NewEncoder(w).Encode(AlertToSend)
-	//get agent_id or email if not the agent_id
-	// fetch all alerted related to user
-
+	utils.WriteJSON(w, http.StatusOK, map[string]string{
+		"message": "alerts fetched successfully",
+	})
+	json.NewEncoder(w).Encode(allAlerts)
 }
 
 // GET /api/alerts/:agent_id
 func (h *AlertType) GetAlertByAgentId(w http.ResponseWriter, r *http.Request) {
-	agentID := chi.URLParam(r, "agent_id")
-	fmt.Println("→ Get alerts for agent:", agentID)
-	// TODO: fetch alerts by agent ID from DB
-	w.Write([]byte("get alerts by agent_id"))
+	ctx := r.Context()
+	userId := r.URL.Query().Get("id")
+	email := r.Context().Value("email").(string)
+	if userId == "" {
+		utils.WriteJSON(w, http.StatusBadRequest, map[string]string{
+			"message": "params id is missing",
+		})
+		return
+	}
+	user, err := h.DB.GetUserByEmail(ctx, email)
+	if utils.CheckError(w, err, http.StatusBadRequest, "user not found") {
+		return
+	}
+	id, err := strconv.ParseInt(userId, 10, 64)
+	if utils.CheckError(w, err, http.StatusBadRequest, "error parsing id") {
+		return
+	}
+
+	params := db.GetAlertByAgentIdParams{
+		ID:      id,
+		AgentID: int64(user.ID),
+	}
+	alert, err := h.DB.GetAlertByAgentId(ctx, params)
+	if errors.Is(err, sql.ErrNoRows) {
+		utils.WriteJSON(w, http.StatusNotFound, map[string]string{
+			"message": "no alert found",
+		})
+		return
+	}
+	if utils.CheckError(w, err, http.StatusBadRequest, "agent or id  not found") {
+		return
+	}
+
+	networkJson := []map[string]any{}
+	err = json.Unmarshal(alert.Network, &networkJson)
+	if utils.CheckError(w, err, http.StatusBadRequest, "error json") {
+		return
+	}
+	securityJson := []map[string]any{}
+	err = json.Unmarshal(alert.Network, &securityJson)
+	if utils.CheckError(w, err, http.StatusBadRequest, "error json") {
+		return
+	}
+	performanceJson := []map[string]any{}
+	err = json.Unmarshal(alert.Network, &performanceJson)
+	if utils.CheckError(w, err, http.StatusBadRequest, "error json") {
+		return
+	}
+	alertToSend := map[string]any{
+		"ID":          alert.ID,
+		"AgentID":     1,
+		"AgentToken":  "abc123xyz",
+		"AlertType":   alert.AlertType,
+		"Severity":    alert.Severity,
+		"Message":     alert.Message,
+		"RawPayload":  alert.RawPayload,
+		"Status":      alert.Status,
+		"RiskLevel":   alert.RiskLevel,
+		"Summary":     alert.Summary,
+		"Performance": performanceJson,
+		"Network":     networkJson,
+		"Security":    securityJson,
+		"CreatedAt":   alert.CreatedAt,
+	}
+	json.NewEncoder(w).Encode(alertToSend)
 }
 
 // PATCH /api/alerts/:id/read
 func (h *AlertType) UpdateAlertsById(w http.ResponseWriter, r *http.Request) {
-	idParam := chi.URLParam(r, "id")
-	alertID, err := strconv.ParseInt(idParam, 10, 64)
+	idQuiery := r.URL.Query().Get("id")
+	alertID, err := strconv.ParseInt(idQuiery, 10, 64)
 
 	if err != nil {
 		http.Error(w, "invalid alert id", http.StatusBadRequest)
