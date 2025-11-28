b9ef66f6cecbd82e063bbfb71f650ed38b5ea6c7
Teklu <tekluabayneh@gmail.com>
2025-11-29 00:20:43 +0900

    Update acknowledge_agent handler

--- backend/internal/handler/acknowledge_agent.go
+++ backend/internal/handler/acknowledge_agent.go
@@ -56,6 +56,7 @@ func (h *DevicePairingType) AcknowledgePairing(w http.ResponseWriter, r *http.Re
 		utils.WriteJSON(w, http.StatusInternalServerError, map[string]any{
 			"message": "internal server error",
 			"ack":     false,
+			"dl":      err.Error(),
 		})
 		return
 	}
