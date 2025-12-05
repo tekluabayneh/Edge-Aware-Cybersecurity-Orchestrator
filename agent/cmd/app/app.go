c0ec1957494bcb6ddcd05262f6cb77034cf8dfac
Teklu <tekluabayneh@gmail.com>
2025-12-06 00:23:57 +0900

    Add new cmd/app folder

---
+++ agent/cmd/app/app.go
@@ -0,0 +1,28 @@
+package app
+
+import (
+	"agent/internal/register"
+	"agent/internal/telemetry"
+	"fmt"
+)
+
+// first the cli cick in
+// the cli get validated if it contain the token
+// the deviceparing api will be called then that will send the token match the token with the sotored token if the toke match  continue
+// then acknowledge api will be alled witht agent info
+// after succeess agent will send raw data to analizer
+// loop will continue forever
+func App() {
+
+	// authenticate agent with user
+	isRegister, err := register.Register()
+	if err != nil {
+		fmt.Println("user not registred")
+		return
+	}
+
+	fmt.Print(isRegister)
+
+	// if register pass start the app
+	telemetry.Telemetry()
+}
