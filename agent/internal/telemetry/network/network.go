2ca56fa4b5e2e385e1bee7bda643b97c18252146
Teklu <tekluabayneh@gmail.com>
2025-12-09 00:57:19 +0900

    Update network telemetry collection

--- agent/internal/telemetry/network/network.go
+++ agent/internal/telemetry/network/network.go
@@ -3,17 +3,27 @@ package network
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func Network(ctx context.Context) {
+func Network(ch chan bool) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var wg sync.WaitGroup
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			fmt.Println("network second")
-			time.Sleep(5 * time.Second)
+			wg.Add(1)
+			go func() {
+				fmt.Println("network second")
+				time.Sleep(5 * time.Second)
+				ch <- true
+				defer wg.Done()
+			}()
+			wg.Wait()
 		}
 	}
 }
