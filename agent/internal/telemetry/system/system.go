6e815c6a1440e51e2c43d6c5fb35b07d9e52bfb5
Teklu <tekluabayneh@gmail.com>
2025-12-09 00:57:19 +0900

    Update system telemetry and GetSysInfotype struct

--- agent/internal/telemetry/system/system.go
+++ agent/internal/telemetry/system/system.go
@@ -1,20 +1,45 @@
 package system
 
 import (
+	"agent/internal/utils"
 	"context"
-	"fmt"
-	"time"
+	"math"
+	"sync"
 )
 
-func System(ctx context.Context) {
+type GetSysInfotype struct {
+	Uptime  string
+	Cpu     []float64
+	Ram     float64
+	Disk    float64
+	Network int64
+}
+
+func System(ch chan GetSysInfotype) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var wg sync.WaitGroup
+
 	for {
 		select {
-
 		case <-ctx.Done():
 			return
 		default:
-			fmt.Println("test system")
-			time.Sleep(5 * time.Second)
+			upTime := utils.GetUptime()
+			cpuInfo := utils.Sysinfo().CPU
+			ramInfo := utils.Sysinfo().RAM
+			diskInfo := utils.Sysinfo().Disk
+			netWorkInfo := utils.Sysinfo().Network
+
+			values := GetSysInfotype{
+				Uptime:  upTime,
+				Cpu:     cpuInfo,
+				Ram:     ramInfo.UsedPercent,
+				Disk:    math.Round(float64(diskInfo.UsedPercent)),
+				Network: netWorkInfo,
+			}
+			ch <- values
+			wg.Wait()
 		}
 	}
 }
