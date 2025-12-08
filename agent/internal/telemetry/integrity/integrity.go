6ba0bbd94bb185a86148e90c22a6bd7b26a33b63
Teklu <tekluabayneh@gmail.com>
2025-12-09 00:57:19 +0900

    Update integrity check logic and result struct

--- agent/internal/telemetry/integrity/integrity.go
+++ agent/internal/telemetry/integrity/integrity.go
@@ -3,17 +3,26 @@ package integrity
 import (
 	"context"
 	"fmt"
+	"sync"
 	"time"
 )
 
-func Integrity(ctx context.Context) {
+func Integrity(ch chan bool) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+	var wg sync.WaitGroup
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		default:
-			fmt.Println("integrity scan")
-			time.Sleep(5 * time.Second)
+			wg.Add(1)
+			go func() {
+				ch <- true
+				fmt.Println("integrity scan")
+				time.Sleep(5 * time.Second)
+			}()
+			wg.Wait()
 		}
 	}
 }
