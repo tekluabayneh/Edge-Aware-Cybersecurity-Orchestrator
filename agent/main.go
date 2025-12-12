7929fd4351e7466d2f7ab0a7c714aaed02f3012e
Teklu <tekluabayneh@gmail.com>
2025-12-13 00:55:40 +0900

    Update main.go in agent

--- agent/main.go
+++ agent/main.go
@@ -3,9 +3,17 @@ package main
 import (
 	"agent/cmd/app"
 	"fmt"
+	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading env")
+	}
+
 	app.App()
 	fmt.Print("main work")
 }
