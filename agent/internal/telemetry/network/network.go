ed904dc7e314b518681e6e55e9f9a2ee1fdb8f0f
Teklu <tekluabayneh@gmail.com>
2025-12-19 02:51:44 +0900

    telemetry(network): improve network monitoring logic

--- agent/internal/telemetry/network/network.go
+++ agent/internal/telemetry/network/network.go
@@ -2,7 +2,8 @@ package network
 
 import (
 	"context"
-	"fmt"
+	// "encoding/json"
+	// "log"
 	"sync"
 	"time"
 )
@@ -18,9 +19,34 @@ func Network(ch chan bool) {
 		default:
 			wg.Add(1)
 			go func() {
-				susIp := FilterSusIp()
-				fmt.Println(susIp)
-				fmt.Println("network second")
+				// checkedIpdsCollectin
+				//  connections monitoring collect
+
+				// check susIp only after ~41 times in an hour
+				// since the time expire use the existing sus ips data
+				lastRun := time.Now()
+				delay := time.Minute * 8
+				if time.Since(lastRun) < delay.Abs() {
+					// susIp, err := FilterSusIp()
+					// if err != nil {
+					// 	log.Println("Warning: no suspicious IPs found or failed to fetch")
+					// }
+					// jsonValue, err := json.Marshal(susIp)
+					// if err != nil {
+					// 	log.Println("Warning: failed to marshal suspicious IPs:", err)
+					// }
+					//
+					// jsonData := map[string]AbuseIPDBResponse{}
+					// err = json.Unmarshal(jsonValue, &jsonData)
+					// if err != nil {
+					// 	log.Println("Warning: failed to unmarshal suspicious IPs:", err)
+					// }
+					// lastRun = time.Now()
+					//
+				}
+
+				ConnectionsMonitoring()
+
 				time.Sleep(5 * time.Second)
 				ch <- true
 				defer wg.Done()
