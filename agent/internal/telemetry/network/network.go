1f0fef4e10b98f747f47941b4c7f85bf5b25fb61
Teklu <tekluabayneh@gmail.com>
2025-12-20 01:57:08 +0900

    Update network interface handling and status checks

--- agent/internal/telemetry/network/network.go
+++ agent/internal/telemetry/network/network.go
@@ -2,8 +2,9 @@ package network
 
 import (
 	"context"
-	// "encoding/json"
-	// "log"
+	"encoding/json"
+	"fmt"
+	"log"
 	"sync"
 	"time"
 )
@@ -27,26 +28,26 @@ func Network(ch chan bool) {
 				lastRun := time.Now()
 				delay := time.Minute * 8
 				if time.Since(lastRun) < delay.Abs() {
-					// susIp, err := FilterSusIp()
-					// if err != nil {
-					// 	log.Println("Warning: no suspicious IPs found or failed to fetch")
-					// }
-					// jsonValue, err := json.Marshal(susIp)
-					// if err != nil {
-					// 	log.Println("Warning: failed to marshal suspicious IPs:", err)
-					// }
-					//
-					// jsonData := map[string]AbuseIPDBResponse{}
-					// err = json.Unmarshal(jsonValue, &jsonData)
-					// if err != nil {
-					// 	log.Println("Warning: failed to unmarshal suspicious IPs:", err)
-					// }
-					// lastRun = time.Now()
-					//
+					susIp, err := FilterSusIp()
+					if err != nil {
+						log.Println("Warning: no suspicious IPs found or failed to fetch", err)
+					}
+					jsonValue, err := json.Marshal(susIp)
+					if err != nil {
+						log.Println("Warning: failed to marshal suspicious IPs:", err)
+					}
+
+					jsonData := map[string]AbuseIPDBResponse{}
+					err = json.Unmarshal(jsonValue, &jsonData)
+					if err != nil {
+						log.Println("Warning: failed to unmarshal suspicious IPs:", err)
+					}
+					lastRun = time.Now()
+
 				}
 
-				ConnectionsMonitoring()
-
+				val := ConnectionsMonitoring()
+				fmt.Printf("data %+v\n", val)
 				time.Sleep(5 * time.Second)
 				ch <- true
 				defer wg.Done()
