91e1364600f020334f43e65ccbb119048fa7a0be
Teklu <tekluabayneh@gmail.com>
2025-12-24 01:21:54 +0900

    security: improve suspicious file scanning

--- agent/internal/telemetry/security/suspicious_files.go
+++ agent/internal/telemetry/security/suspicious_files.go
@@ -1,13 +1,82 @@
 package security
 
-import "fmt"
+import (
+	"agent/internal/utils"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
 
-type SuspiciousFile struct {
-	Path   string
-	Reason string
+type SuspiciouseFiletype struct {
+	Path      string      `json:"path"`
+	Extension string      `json:"extension"`
+	Name      string      `json:"name"`
+	Size      int64       `json:"size"`
+	Mode      fs.FileMode `json:"mode"`
+	Content   string      `json:"content"`
 }
 
-func DetectSuspiciousFiles() []SuspiciousFile {
-	fmt.Println("DetectSuspiciousFiles")
-	return []SuspiciousFile{}
+// start form HomeDir and read all fiels
+func DetectSuspiciousFiles() []SuspiciouseFiletype {
+	var SuspiciousFile []SuspiciouseFiletype
+	HomeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Printf("error reading home directory")
+	}
+
+	// walk all the fils but only grap file with cetain extenstion
+	// and collect files with their info
+	filepath.WalkDir(HomeDir, func(path string, d fs.DirEntry, err error) error {
+		if d.IsDir() {
+			if utils.SkipDirs[d.Name()] {
+				return filepath.SkipDir
+			}
+		}
+
+		if !d.IsDir() {
+			name := strings.Split(d.Name(), ".")
+			if len(name) > 1 {
+				if utils.FilesNameToCheckAgainst[name[1]] {
+					content, err := os.ReadFile(path)
+					if err != nil {
+						log.Printf("error reading home directory")
+					}
+					// info, err := d.Info()
+					if err != nil {
+						log.Printf("error reading home directory")
+					}
+
+					jsonData, err := json.Marshal(content)
+					fmt.Println("json", string(jsonData))
+					if err != nil {
+						log.Printf("error reading home directory")
+					}
+
+					var ContentData string
+					json.Unmarshal(jsonData, &ContentData)
+					// fmt.Println("expected data", string(ContentData))
+					if err != nil {
+						ContentInfo := SuspiciouseFiletype{
+							// Name:      d.Name(),
+							// Path:      path,
+							// Extension: strings.Split(d.Name(), ".")[1],
+							// Size:      info.Size(),
+							// Mode:      info.Mode(),
+							Content: string(content),
+						}
+
+						SuspiciousFile = append(SuspiciousFile, ContentInfo)
+					}
+				}
+			}
+		}
+		return nil
+	})
+
+	fmt.Println(SuspiciousFile)
+	return SuspiciousFile
 }
