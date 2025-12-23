9010a7467469685ad653d37b77443be8311a5e51
Teklu <tekluabayneh@gmail.com>
2025-12-24 01:21:54 +0900

    security: update malicious process detection

--- agent/internal/telemetry/security/malicious_processes.go
+++ agent/internal/telemetry/security/malicious_processes.go
@@ -1,14 +1,42 @@
 package security
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/shirou/gopsutil/v3/process"
+)
 
 type SuspiciousProcess struct {
-	PID    int32
-	Name   string
-	Reason string
+	PID      int32
+	Name     string
+	Path     string
+	UserName string
 }
 
 func DetectMaliciousProcesses() []SuspiciousProcess {
-	fmt.Println("DetectMaliciousProcesses")
-	return []SuspiciousProcess{}
+	procs, _ := process.Processes()
+	var SuspiciousProcessData []SuspiciousProcess
+
+	for _, proc := range procs {
+		cmd, err := proc.Cmdline()
+		if err != nil {
+			log.Printf(
+				"failed to get cmdline for pid %d: %v",
+				proc.Pid,
+				err,
+			)
+			continue
+		}
+
+		name, _ := proc.Name()
+		username, _ := proc.Username()
+		proccessInfo := SuspiciousProcess{
+			Name:     name,
+			PID:      proc.Pid,
+			UserName: username,
+			Path:     cmd,
+		}
+		SuspiciousProcessData = append(SuspiciousProcessData, proccessInfo)
+	}
+	return SuspiciousProcessData
 }
