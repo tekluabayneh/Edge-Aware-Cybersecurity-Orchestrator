33e6aa03182886c98a039cddab6447a012d51fc0
Teklu <tekluabayneh@gmail.com>
2025-12-26 01:04:15 +0900

    Update telemetry.go with new telemetry aggregation logic

--- agent/internal/telemetry/telemetry.go
+++ agent/internal/telemetry/telemetry.go
@@ -2,11 +2,12 @@ package telemetry
 
 import (
 	// "agent/internal/telemetry/integrity"
-	// "agent/internal/telemetry/network"
-	// "agent/internal/telemetry/processes"
+	"agent/internal/telemetry/network"
+	"agent/internal/telemetry/processes"
 	"agent/internal/telemetry/security"
-	// "agent/internal/telemetry/system"
+	"agent/internal/telemetry/system"
 	"context"
+	"fmt"
 	"sync"
 	"time"
 )
@@ -15,10 +16,10 @@ func Telemetry() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	var wg sync.WaitGroup
-	// chanSystemInfo := make(chan system.GetSysInfotype)
+	chanSystemInfo := make(chan system.GetSysInfotype)
 	chanSecurity := make(chan security.SecurityReport)
-	// chanNetwork := make(chan bool)
-	// chanProcesses := make(chan bool)
+	chanNetwork := make(chan network.ConnectionMonitoringType)
+	chanProcesses := make(chan []processes.ProcInfo)
 	// chanIntegrity := make(chan bool)
 
 	for {
@@ -30,22 +31,23 @@ func Telemetry() {
 			go func() {
 
 				go security.Security(chanSecurity)
-				// go network.Network(chanNetwork)
-				// go processes.Processes(chanProcesses)
+				go network.Network(chanNetwork)
+				go processes.Processes(chanProcesses)
 				// go integrity.Integrity(chanIntegrity)
-				// go system.System(chanSystemInfo)
+				go system.System(chanSystemInfo)
 
 				// Collect system info
-				// sysInfo := <-chanSystemInfo
-				// securityr := <-chanSecurity
-				// networkr := <-chanNetwork
-				// processr := <-chanProcesses
+				sysInfo := <-chanSystemInfo
+				securityr := <-chanSecurity
+				networkr := <-chanNetwork
+				processr := <-chanProcesses
 				// integR := <-chanIntegrity
+
 				//
-				// fmt.Println(sysInfo)
-				// fmt.Println(securityr)
-				// fmt.Println(networkR)
-				// fmt.Println(processr)
+				fmt.Println(sysInfo)
+				fmt.Println(securityr)
+				fmt.Println(networkr)
+				fmt.Println(processr)
 				// fmt.Println(integr)
 
 				// send all alert to analizer
