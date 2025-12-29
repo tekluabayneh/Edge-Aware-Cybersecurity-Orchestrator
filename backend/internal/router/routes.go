5789cdf8ad63b97b4b7047c45bb6c4546064df3b
Teklu <tekluabayneh@gmail.com>
2025-12-30 02:29:13 +0900

    update API routes configuration

--- backend/internal/router/routes.go
+++ backend/internal/router/routes.go
@@ -76,7 +76,7 @@ func CreateCommand(router chi.Router, db *db.Queries) {
 
 func FeatchCommand(router chi.Router, db *db.Queries) {
 	SingleAlertHandler := &handler.CreateCommdnType{DB: db}
-	router.Get("/fetch", SingleAlertHandler.FetchPendingCommandsHandler)
+	router.Post("/fetch", SingleAlertHandler.FetchPendingCommandsHandler)
 }
 
 func AcknowledgeCommandExecutionHandle(router chi.Router, db *db.Queries) {
