66adb23ec6dba90fc5ea4fe52e9c67d0257ca6e2
Teklu <tekluabayneh@gmail.com>
2026-01-01 00:02:40 +0900

    Improve telemetry handling

--- agent/internal/telemetry/telemetry.go
+++ agent/internal/telemetry/telemetry.go
@@ -2,11 +2,11 @@ package telemetry
 
 import (
 	"agent/internal/commands"
-	"agent/internal/telemetry/integrity"
-	"agent/internal/telemetry/network"
-	"agent/internal/telemetry/processes"
-	"agent/internal/telemetry/security"
-	"agent/internal/telemetry/system"
+	// "agent/internal/telemetry/integrity"
+	// "agent/internal/telemetry/network"
+	// "agent/internal/telemetry/processes"
+	// "agent/internal/telemetry/security"
+	// "agent/internal/telemetry/system"
 	"context"
 	"fmt"
 	"sync"
@@ -17,11 +17,11 @@ func Telemetry() {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	var wg sync.WaitGroup
-	chanSystemInfo := make(chan system.GetSysInfotype)
-	chanSecurity := make(chan security.SecurityReport)
-	chanNetwork := make(chan network.NetworkSnapshot)
-	chanProcesses := make(chan []processes.ProcInfo)
-	chanIntegrity := make(chan integrity.IntegritySnapshot)
+	// chanSystemInfo := make(chan system.GetSysInfotype)
+	// chanSecurity := make(chan security.SecurityReport)
+	// chanNetwork := make(chan network.NetworkSnapshot)
+	// chanProcesses := make(chan []processes.ProcInfo)
+	// chanIntegrity := make(chan integrity.IntegritySnapshot)
 	chanCommands := make(chan commands.CommandType)
 	for {
 		select {
@@ -31,11 +31,11 @@ func Telemetry() {
 			wg.Add(1)
 			go func() {
 
-				go security.Security(chanSecurity)
-				go network.Network(chanNetwork)
-				go processes.Processes(chanProcesses)
-				go integrity.Integrity(chanIntegrity)
-				go system.System(chanSystemInfo)
+				// go security.Security(chanSecurity)
+				// go network.Network(chanNetwork)
+				// go processes.Processes(chanProcesses)
+				// go integrity.Integrity(chanIntegrity)
+				// go system.System(chanSystemInfo)
 				go commands.Commands(chanCommands)
 
 				// Collect system info
