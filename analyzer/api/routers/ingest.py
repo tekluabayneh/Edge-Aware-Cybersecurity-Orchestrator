033fac8f1ab068ba6c47bd6370e26231594bc2dc
Teklu <tekluabayneh@gmail.com>
2026-01-06 01:51:04 +0900

    feat: update ingest router

--- analyzer/api/routers/ingest.py
+++ analyzer/api/routers/ingest.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
-from  ..schemas.Telementory import TelemetryPayload
+from  ..schemas.Telementory import RawTelemetryPayload
 router = APIRouter(tags=["ingest"])
 
 
 @router.post("/rawTelementory")
-def get_activity_for_user(payload: TelemetryPayload):
-    print(payload)
+def get_activity_for_user(payload: RawTelemetryPayload):
+    print("this is paylod man paylod",payload)
     print("get activity")
     return {"msg": "get data"}
 
