94029e4203d3da978a0ff425cdbf352fc42f12d4
Teklu <tekluabayneh@gmail.com>
2026-01-11 01:57:32 +0900

    Update main analyzer entrypoint

--- analyzer/main.py
+++ analyzer/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI, Request
 from api.api import api_router
 from starlette.middleware.cors import CORSMiddleware
-from fastapi.responses import JSONResponse
-from fastapi.exceptions import RequestValidationError
-
 
 
 @api_router.get("/")
@@ -13,19 +10,6 @@ def check():
 
 app = FastAPI(title="Analyzer API")
 app.include_router(api_router, prefix="/api/v1")
-
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, res: RequestValidationError):
-    print(res.body)
-    return JSONResponse(
-        status_code=422,
-        content={
-            "detail": res.errors(),
-            "body": res.body,
-        },
-    )
-
-
 app.add_middleware(
         CORSMiddleware,
         allow_origins=["*"],
