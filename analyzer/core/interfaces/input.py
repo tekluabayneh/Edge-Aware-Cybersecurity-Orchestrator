c211b9cb80d1c5bef39daad41ffe821c8fb4e448
Teklu <tekluabayneh@gmail.com>
2026-01-13 02:10:17 +0900

    Update input interface definitions

--- analyzer/core/interfaces/input.py
+++ analyzer/core/interfaces/input.py
@@ -3,11 +3,11 @@ from core.interfaces.normalizer import integrity, network, processes, security, 
 
 # send each raw data to their own normalizer
 def Input(payload):
-    print(payload.email) 
-    print(payload.agent_id) 
-    print(payload.agent_token)
     network.netwrok_normalizer(payload.network)
     processes.processes_normalizer(payload.processes)
     integrity.intgerity_normalizer(payload.integrity)
     system.system_normalizer(payload.system)
     security.security_normalizer(payload.security)
+   
+
+
