ecaaae90571c6936d444dbe3a78e11f5de966f5e
Teklu <tekluabayneh@gmail.com>
2026-01-13 02:10:17 +0900

    Enhance system information normalization

--- analyzer/core/interfaces/normalizer/system.py
+++ analyzer/core/interfaces/normalizer/system.py
@@ -1,3 +1,13 @@
 
-def system_normalizer(systemPaylod: dict): 
-     print("===========sys=========",systemPaylod,"==============sys-e===========")
+from shared.schemas.schema import Event
+
+
+def system_normalizer(systemPaylod: dict) -> Event: 
+     return Event(
+            payload=systemPaylod,
+            type="system",
+            category="system", 
+            source="agent",
+            tags=["system"]
+            )
+  
