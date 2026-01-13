90176fb97c7411adec361a1f009f7a048dcb841f
Teklu <tekluabayneh@gmail.com>
2026-01-14 01:19:46 +0900

    Improve processes normalizer

--- analyzer/core/interfaces/normalizer/processes.py
+++ analyzer/core/interfaces/normalizer/processes.py
@@ -1,12 +1,11 @@
-
-
+from fastapi.encoders import jsonable_encoder
 from shared.schemas.schema import Event
 
 
 def processes_normalizer(processesPaylod: dict) -> Event: 
-
+     payload =jsonable_encoder(processesPaylod) 
      return Event(
-            payload=processesPaylod,
+            payload=payload,
             type="processes",
             category="processes", 
             source="agent",
