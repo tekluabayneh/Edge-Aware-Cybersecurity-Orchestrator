5bf917358e12f094fa4cd4fd7ee310c84a8d458e
Teklu <tekluabayneh@gmail.com>
2026-01-14 01:19:46 +0900

    Enhance security normalizer

--- analyzer/core/interfaces/normalizer/security.py
+++ analyzer/core/interfaces/normalizer/security.py
@@ -1,12 +1,10 @@
-
-
+from fastapi.encoders import jsonable_encoder
 from shared.schemas.schema import Event
 
-
 def security_normalizer(securityPaylod: dict) -> Event: 
-    
+     payload =jsonable_encoder(securityPaylod) 
      return Event(
-            payload=securityPaylod,
+            payload=payload,
             type="security",
             category="security", 
             source="agent",
