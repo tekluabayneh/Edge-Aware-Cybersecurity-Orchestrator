ab6bf62cdfefec4208bf2802df8b727ff90bb075
Teklu <tekluabayneh@gmail.com>
2026-01-14 01:19:46 +0900

    Update system normalizer

--- analyzer/core/interfaces/normalizer/system.py
+++ analyzer/core/interfaces/normalizer/system.py
@@ -1,10 +1,11 @@
-
+from fastapi.encoders import jsonable_encoder
 from shared.schemas.schema import Event
 
 
 def system_normalizer(systemPaylod: dict) -> Event: 
+     payload =jsonable_encoder(systemPaylod) 
      return Event(
-            payload=systemPaylod,
+            payload=payload,
             type="system",
             category="system", 
             source="agent",
