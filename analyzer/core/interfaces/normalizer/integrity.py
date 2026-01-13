feaca1d1a7e263c586430fad332c4fd82a4ef801
Teklu <tekluabayneh@gmail.com>
2026-01-14 01:19:46 +0900

    Enhance integrity normalizer

--- analyzer/core/interfaces/normalizer/integrity.py
+++ analyzer/core/interfaces/normalizer/integrity.py
@@ -1,11 +1,14 @@
 from shared.schemas.schema import Event
+from fastapi.encoders import jsonable_encoder
 
 def intgerity_normalizer(intgerityPaylod) -> Event: 
+    payload =jsonable_encoder(intgerityPaylod) 
     return Event(
-            payload=intgerityPaylod,
+            payload=payload,
             type="integrity",
             category="integrity", 
             source="agent",
             tags=["integrity"]
             )
+
     
