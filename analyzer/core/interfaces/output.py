8d3488bdb6fc5b9aa140f504e7ef180b27ac9f47
Teklu <tekluabayneh@gmail.com>
2026-02-11 17:14:55 +0900

    update output interface to include processed timestamps

--- analyzer/core/interfaces/output.py
+++ analyzer/core/interfaces/output.py
@@ -1,3 +1,4 @@
 import json
 def send_output(event): 
+    #output is responseible for updading or formating how the output data look like
     print("Clean payload going to pipeline:\n" + json.dumps(event, indent=2))
