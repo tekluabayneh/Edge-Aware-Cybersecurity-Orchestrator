6201a2469bf9fb3e382b979f79cf734f879bfb7b
Teklu <tekluabayneh@gmail.com>
2026-02-11 17:14:55 +0900

    adjust input interface for context and telemetry updates

--- analyzer/core/interfaces/input.py
+++ analyzer/core/interfaces/input.py
@@ -1,16 +1,16 @@
 from core.interfaces.normalizer import integrity, network, processes, security, system
-
+import json
 
 def Input(payload):
      try: 
       result = {
-        "network":   network.netwrok_normalizer(payload.get("network")),
-        "processes": processes.processes_normalizer(payload.get("processes")),
-        "system":    system.system_normalizer(payload.get("system")),
-        "integrity": integrity.intgerity_normalizer(payload.get("integrity")),
-        "security":  security.security_normalizer(payload.get("security")),
+        "network":   network.netwrok_normalizer(payload.raw_payload.get("network")),
+        "processes": processes.processes_normalizer(payload.raw_payload.get("processes")),
+        "system":    system.system_normalizer(payload.raw_payload.get("system")),
+        "integrity": integrity.intgerity_normalizer(payload.raw_payload.get("integrity")),
+        "security":  security.security_normalizer(payload.raw_payload.get("security")),
         } 
-     
+
       return result
      except AttributeError as e: 
         print("ERROR in Input():", str(e))
