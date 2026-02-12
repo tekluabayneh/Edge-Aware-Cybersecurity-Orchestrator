f62b93a486db0c12228cfff7cd44ca9f7ab50b71
Teklu <tekluabayneh@gmail.com>
2026-02-12 16:03:41 +0900

    Update ingest router to pass events to pipeline for alert processing

--- analyzer/api/routers/ingest.py
+++ analyzer/api/routers/ingest.py
@@ -1,38 +1,3 @@
-# from core.engine.pipeline import piplinejob
-# from fastapi import APIRouter, Request
-# from  ..schemas.Telementory import RawTelemetryPayload
-# from core.context.analyzer_context import AnalyzerContext
-# import json
-# router = APIRouter(tags=["ingest"])
-#
-# @router.post("/rawTelementory")
-# async def get_activity_for_user(payload: RawTelemetryPayload, req:Request):
-#         payload_dict = {
-#         "email": payload.email,
-#         "agent_id": payload.agent_id,
-#         "agent_token": payload.agent_token,
-#         "processes": [
-#             proc.model_dump(mode='json')   
-#             for proc in payload.processes
-#         ],
-#         "network":   payload.network.model_dump(mode='json'),
-#         "system":    payload.system.model_dump(mode='json'),
-#         "integrity": payload.integrity.model_dump(mode='json'),
-#         "security":  payload.security.model_dump(mode='json'),
-#          }
-#         context = AnalyzerContext()
-#         user = {
-#                 "email":payload.get("email"),
-#                 "agent_id":payload.get("agent_id"),
-#                 "agent_token":payload.get("agent_token"),
-#                 }
-#
-#         context.user =  user
-#
-#         # print("Clean payload going to pipeline:\n" + json.dumps(payload_dict, indent=2))
-#
-#         piplinejob(payload_dict)
-
 from datetime import datetime, timezone
 from fastapi import APIRouter, Request, HTTPException
 from core.engine.pipeline import piplinejob   
@@ -67,7 +32,6 @@ async def ingest_raw_telemetry(payload: RawTelemetryPayload, req: Request):
 
     # Better request tracing
     context.request_id = str(uuid.uuid4())
-    # context.start_time = req.state.start_time if hasattr(req.state, 'start_time') else None 
     context.start_time = getattr(req.state, "start_time", None)
 
     # Attach user/agent info (make it a proper dict or object if you want later)
