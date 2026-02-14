8d668f2dd6ee139248a7facfa3eb2674169ff324
Teklu <tekluabayneh@gmail.com>
2026-02-14 16:17:09 +0900

    update alert handler interface for plugin integration

--- analyzer/core/interfaces/alert_handler.py
+++ analyzer/core/interfaces/alert_handler.py
@@ -1,23 +1,20 @@
 import requests
-
-def send_alert(alert, context):
+import json
+import os
+def send_alert(alert):
     """
     Add user/agent info and send to backend
     """
-    payload = {
-        "rule_id": alert["rule_id"],
-        "severity": alert["severity"],
-        "title": alert["title"],
-        "event": alert["event"],
-        "user_id": context.user_id,
-        "email": context.email,
-        "agent_id": context.agent_id,
-        "agent_token": context.agent_token
-    }
-
+    print("Clean payload going to pipeline in send:\n" + json.dumps(alert, indent=2))
+    
     try:
-        response = requests.post("https://backend/api/alert", json=payload)
+        BASE_URL = os.getenv("BACKEND_BASE_URL") 
+        if BASE_URL is None: 
+            print("BACKEND_BASE_URL not found")
+            return
+        response = requests.post(f"{BASE_URL}/newAlert/create", json=alert,  headers={"Content-Type": "application/json"})
+        print("api response",response)
         response.raise_for_status()
-        print(f"Alert sent successfully: {payload['rule_id']}")
+        print(f"Alert sent successfully")
     except Exception as e:
         print(f"Failed to send alert: {e}")
