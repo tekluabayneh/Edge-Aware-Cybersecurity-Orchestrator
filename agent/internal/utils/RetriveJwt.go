a1f76a9e1d13daa1749f9dcb4ded7959288107cb
Teklu <tekluabayneh@gmail.com>
2026-02-17 15:30:14 +0900

    feat(agent): add utility for retrieving JWT from storage

---
+++ agent/internal/utils/RetriveJwt.go
@@ -0,0 +1,30 @@
+package utils
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+type tokentype string
+
+func Getjwt() (string, error) {
+	tokenpath := filepath.Join("internal/register", "Jwttoken.txt")
+	content, err := os.ReadFile(tokenpath)
+	if err != nil {
+		log.Printf("failed to write token file %s: %v", tokenpath, err)
+		return "", errors.New("error while reading file")
+	}
+	//
+	token := strings.TrimSpace(string(content))
+	fmt.Println(token)
+	if token == "" {
+		log.Println("received empty token from server")
+		return "", errors.New("error while reading file")
+	}
+	return token, nil
+}
+
