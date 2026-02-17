b34a6af61acf2ab7a0836d854a124a944bfec2ba
Teklu <tekluabayneh@gmail.com>
2026-02-17 15:30:14 +0900

    refactor(analyzer): update alert handler interface

--- analyzer/core/interfaces/alert_handler.py
+++ analyzer/core/interfaces/alert_handler.py
@@ -5,15 +5,15 @@ def send_alert(alert):
     """
     Add user/agent info and send to backend
     """
-    print("Clean payload going to pipeline in send:\n" + json.dumps(alert, indent=2))
-    
     try:
         BASE_URL = os.getenv("BACKEND_BASE_URL") 
         if BASE_URL is None: 
-            print("BACKEND_BASE_URL not found")
+            print("BACKEND_BASE_URL not found", BASE_URL)
             return
+
         response = requests.post(f"{BASE_URL}/newAlert/create", json=alert,  headers={"Content-Type": "application/json"})
-        print("api response",response)
+        print("api response",response.json().get("message"))
+
         response.raise_for_status()
         print(f"Alert sent successfully")
     except Exception as e:
