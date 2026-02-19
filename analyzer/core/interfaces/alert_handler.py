4c57701d47294c0c4e8c771df54ebc2ab11e72a3
Teklu <tekluabayneh@gmail.com>
2026-02-19 20:08:31 +0900

    fix: update alert_handler interface

--- analyzer/core/interfaces/alert_handler.py
+++ analyzer/core/interfaces/alert_handler.py
@@ -1,19 +1,24 @@
 import requests
 import json
 import os
-def send_alert(alert):
+def send_alert(alert, token):
     """
     Add user/agent info and send to backend
     """
+    print(alert)
     try:
         BASE_URL = os.getenv("BACKEND_BASE_URL") 
         if BASE_URL is None: 
             print("BACKEND_BASE_URL not found", BASE_URL)
             return
 
-        response = requests.post(f"{BASE_URL}/newAlert/create", json=alert,  headers={"Content-Type": "application/json"})
-        print("api response",response.json().get("message"))
 
+        response = requests.post(f"{BASE_URL}/newAlert/create", json=alert,  headers={
+            "Content-Type": "application/json",
+            "Authorization":"Bearer " + token
+            })
+
+        print("api response",response.json().get("message"))
         response.raise_for_status()
         print(f"Alert sent successfully")
     except Exception as e:
