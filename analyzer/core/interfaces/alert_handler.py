9c4ac20e69db9c29b40e48ead34b60f8aeb2af1f
Teklu <tekluabayneh@gmail.com>
2026-02-21 00:56:24 +0900

    Update alert handler interface

--- analyzer/core/interfaces/alert_handler.py
+++ analyzer/core/interfaces/alert_handler.py
@@ -5,7 +5,6 @@ def send_alert(alert, token):
     """
     Add user/agent info and send to backend
     """
-    print(alert)
     try:
         BASE_URL = os.getenv("BACKEND_BASE_URL") 
         if BASE_URL is None: 
@@ -18,8 +17,6 @@ def send_alert(alert, token):
             "Authorization":"Bearer " + token
             })
 
-        print("api response",response.json().get("message"))
         response.raise_for_status()
-        print(f"Alert sent successfully")
     except Exception as e:
         print(f"Failed to send alert: {e}")
