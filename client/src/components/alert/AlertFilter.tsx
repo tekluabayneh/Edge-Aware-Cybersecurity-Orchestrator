e6830587605d6ac56ee1afa8a5014a440982e7b8
Teklu <tekluabayneh@gmail.com>
2026-02-24 19:59:50 +0900

    feat(client): implement alert fetching and filtering logic

--- client/src/components/alert/AlertFilter.tsx
+++ client/src/components/alert/AlertFilter.tsx
@@ -1,26 +1,31 @@
 import { Filter } from 'lucide-react'
 import { filters } from '../../data/filters'
+import GetAllAlerts from "../../hooks/fetchAlerts"
 
 type StateType = {
-  setActiveFilter: React.Dispatch<React.SetStateAction<string>>
+    setActiveFilter: React.Dispatch<React.SetStateAction<string>>
 }
+
+const Alerts = await GetAllAlerts()
+console.log("thisis the data", Alerts)
+
 const AlertFilters: React.FC<StateType> = ({ setActiveFilter }) => {
-  return (
-    <div className="flex flex-wrap gap-3">
-      <div className="flex items-center gap-2 text-gray-400">
-        <Filter className="w-4 h-4" />
-        <span className="text-sm font-medium">Filter by Severity:</span>
-      </div>
-      {filters.map((filter) => (
-        <button
-          key={filter.value}
-          onClick={() => setActiveFilter(filter.value)}
-          className="transition-all duration-200 cursor-pointer rounded px-3 py-1 border  border-purple-900 bg-gray-800  text-gray-400 hover:bg-gray-700 hover:text-gray-200"
-        >
-          {filter.label}
-        </button>
-      ))}
-    </div>
-  )
+    return (
+        <div className="flex flex-wrap gap-3">
+            <div className="flex items-center gap-2 text-gray-400">
+                <Filter className="w-4 h-4" />
+                <span className="text-sm font-medium">Filter by Severity:</span>
+            </div>
+            {filters.map((filter) => (
+                <button
+                    key={filter.value}
+                    onClick={() => setActiveFilter(filter.value)}
+                    className="transition-all duration-200 cursor-pointer rounded px-3 py-1 border  border-purple-900 bg-gray-800  text-gray-400 hover:bg-gray-700 hover:text-gray-200"
+                >
+                    {filter.label}
+                </button>
+            ))}
+        </div>
+    )
 }
 export default AlertFilters
