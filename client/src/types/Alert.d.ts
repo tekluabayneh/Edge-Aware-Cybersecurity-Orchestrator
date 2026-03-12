a1f1a51647104a3c02e3cfb5608e197bbe9426e6
Teklu <tekluabayneh@gmail.com>
2026-03-12 15:26:53 +0900

    client: update alert types

--- client/src/types/Alert.d.ts
+++ client/src/types/Alert.d.ts
@@ -1,44 +1,60 @@
 import type { number } from "framer-motion"
 
 export type AlertType = {
-  id: number
-  title: string
-  description: string
-  severity: string
-  status: string
-  detected_at: string
-  source_ip: string
-  target: string
+    CreatedAt: string
+    agent_id: string
+    agent_token: string
+    alertType: string
+    message: string
+    netetwork: object
+    performance: object
+    risk_level: string
+    security: string
+    status: string
+    summery: string
+}
+
+export type AlertType = {
+    id: number
+    title: string
+    description: string
+    severity: string
+    status: string
+    detected_at: string
+    source_ip: string
+    target: string
 }
 
 export type userType = {
-  email: string
-  full_name: string
-  created_date: string
+    email: string
+    full_name: string
+    created_date: string
 }
 
+
 type MetricCardProps = {
-  title: string
-  value: string | number
-  change: string
-  trend: 'up' | 'down'
-  icon: React.ComponentType
+    title: string
+    value: string | number
+    change: string
+    trend: 'up' | 'down'
+    icon: React.ComponentType
 }
 
 export type DeviceType = {
-  user_id: number,
-  machine_id: number,
-  agent_version: string,
-  os:string,
-  status: string,
-  last_seen:string,
-  created_at:string,
+AgentID:string,
+AgentToken:string
+AgentVersion :string 
+CreatedAt:string
+DeviceName:string 
+ID:number 
+LastSeen:string 
+MachineID:string
+Os:string
+Status:string
+UserID: string
 }
 
-type FormDataType ={
-    password:string,
-    email: string
-}
-type RegisterRequest = FormDataType & {
-  username: string;
-};
+
+
+
+
