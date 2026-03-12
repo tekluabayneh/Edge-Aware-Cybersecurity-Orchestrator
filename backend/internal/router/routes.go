74333674b6e94543276067e74aa61945fac3a4c0
Teklu <tekluabayneh@gmail.com>
2026-03-12 15:26:53 +0900

    router: update application routes

--- backend/internal/router/routes.go
+++ backend/internal/router/routes.go
@@ -4,6 +4,7 @@ import (
 	db "github.com/edge-aware-cyberSecurity/db/sqlc"
 	AuthPath "github.com/edge-aware-cyberSecurity/internal/auth"
 	"github.com/edge-aware-cyberSecurity/internal/handler"
+	"github.com/edge-aware-cyberSecurity/internal/handler/otp"
 	"github.com/go-chi/chi"
 )
 
@@ -38,6 +39,11 @@ func AcknowledgePairing(router chi.Router, db *db.Queries) {
 func TelemetryReport(router chi.Router, db *db.Queries) {
 	TelemetryReportHandler := &handler.TelemetryType{DB: db}
 	router.Post("/report", TelemetryReportHandler.ReceiveTelemetry)
+}
+
+func GetLatestTelemetryData(router chi.Router, db *db.Queries) {
+	TelemetryReportHandler := &handler.TelemetryType{DB: db}
+	router.Get("/telemetry", TelemetryReportHandler.GetLatestTelemetryData)
 }
 
 func AlertReport(router chi.Router, db *db.Queries) {
@@ -93,4 +99,34 @@ func AcknowledgeCommandExecutionHandle(router chi.Router, db *db.Queries) {
 func AcknowledgeCommandExecutionResponseHandle(router chi.Router, db *db.Queries) {
 	SingleAlertHandler := &handler.CreateCommdnType{DB: db}
 	router.Post("/ack", SingleAlertHandler.AcknowledgeCommandExecutionResponseHandler)
+}
+
+func UPdateUserProfile(router chi.Router, db *db.Queries) {
+	UpdateUserProfileHandler := &handler.UserType{DB: db}
+	router.Post("/profile", UpdateUserProfileHandler.UpdateUserInfo)
+}
+
+func GetUserProfile(router chi.Router, db *db.Queries) {
+	GetUserProfileHandler := &handler.UserType{DB: db}
+	router.Get("/profile", GetUserProfileHandler.GetUserProfileInfo)
+}
+
+func GetAllAgent(router chi.Router, db *db.Queries) {
+	GetAllAgentHandler := &handler.AgentType{DB: db}
+	router.Get("/agent", GetAllAgentHandler.GetAllUserAgents)
+}
+
+func Enable2FA(router chi.Router, db *db.Queries) {
+	TowFaeHandler := &otp.TowFaType{DB: db}
+	router.Post("/2fa", TowFaeHandler.Enable2FA)
+}
+
+func Confirm2FA(router chi.Router, db *db.Queries) {
+	TowFaeHandler := &otp.TowFaType{DB: db}
+	router.Post("/2fa", TowFaeHandler.Confirm2FA)
+}
+
+func Verify2FA(router chi.Router, db *db.Queries) {
+	TowFaeHandler := &otp.TowFaType{DB: db}
+	router.Post("/verify", TowFaeHandler.Verify2FA)
 }
