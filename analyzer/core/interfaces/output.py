48d6ec428902a3f630b914e96e2764009387cbcb
Teklu <tekluabayneh@gmail.com>
2026-03-12 15:26:52 +0900

    analyzer: update output interface

--- analyzer/core/interfaces/output.py
+++ analyzer/core/interfaces/output.py
@@ -1,21 +1,29 @@
 import json
 import os
 
-from requests import request
+import requests 
 def send_output(event, token): 
-  try:
-    BASEURL = os.getenv("BASE_URL")
-    if BASEURL is None: 
+   try:
+     BASEURL = os.getenv("BACKEND_BASE_URL")
+     print("base url",BASEURL)
+     if BASEURL is None: 
         return 
 
-        response = requests.post(f"{BASEURL}/api/telementary/report", json=event,  headers={
-            "Content-Type": "application/json",
+     print(event.get("email"))
+     print(event.get("agent_id"))
+     print(event.get("agent_token"))
+
+     response = requests.post(f"{BASEURL}/api/telementary/report", json=event,  headers={
+      "Content-Type": "application/json",
             "Authorization":"Bearer " + token
-            })
+      })
+     print(response.status_code)
+     print(response.text)
+     # response.raise_for_status()
+     # print("Success:", response.json().get("message", "No message key found"))
 
-        response.raise_for_status()
-  except Exception as e: 
-      print(e)
+   except Exception as e: 
+      print("error",e)
 
 
-# print("Clean payload going to pipeline:\n" + json.dumps(event, indent=2))
+   # print("Clean payload going to pipeline:\n" + json.dumps(event.get("integ"), indent=2))
