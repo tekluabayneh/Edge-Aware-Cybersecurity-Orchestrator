00ab11173756c7aff2f4b1d8a4c9c65d297e878b
Teklu <tekluabayneh@gmail.com>
2026-03-12 15:26:53 +0900

    notification: update notification worker

--- backend/internal/handler/notification/notification.wroker.go
+++ backend/internal/handler/notification/notification.wroker.go
@@ -1,1 +1,33 @@
 package notification
+
+import (
+	"context"
+	"os"
+
+	sib_api_v3_sdk "github.com/sendinblue/APIv3-go-library/lib"
+)
+
+func EmailSender(senderName string, senderEmail string, recipientEmail string, subject string, htmlContent string) error {
+
+	apiKey := os.Getenv("BREVO_API_KEY")
+
+	cfg := sib_api_v3_sdk.NewConfiguration()
+	cfg.AddDefaultHeader("api-key", apiKey)
+
+	client := sib_api_v3_sdk.NewAPIClient(cfg)
+
+	email := sib_api_v3_sdk.SendSmtpEmail{
+		Sender: &sib_api_v3_sdk.SendSmtpEmailSender{
+			Name:  senderName,
+			Email: senderEmail,
+		},
+		To: []sib_api_v3_sdk.SendSmtpEmailTo{
+			{Email: recipientEmail},
+		},
+		Subject:     subject,
+		HtmlContent: htmlContent,
+	}
+
+	_, _, err := client.TransactionalEmailsApi.SendTransacEmail(context.Background(), email)
+	return err
+}
