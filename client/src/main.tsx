7e4ca602493cee4716272df7a91682c460c428e1
Teklu <tekluabayneh@gmail.com>
2026-03-12 15:26:53 +0900

    client: update application entry point

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,13 +5,13 @@ import './index.css'
 import App from './App.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient();
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <BrowserRouter>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-    </BrowserRouter>
-    ,
-  </StrictMode>
+    <StrictMode>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <App />
+            </QueryClientProvider>
+        </BrowserRouter>
+    </StrictMode>
 )
