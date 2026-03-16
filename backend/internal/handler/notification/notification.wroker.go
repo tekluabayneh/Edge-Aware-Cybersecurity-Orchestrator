ba3fbe918e832ad4fc0c7e3b4ca5128ef8ff932f
Teklu <tekluabayneh@gmail.com>
2026-03-16 18:56:25 +0900

    Correct worker logic in notification.wroker.go

--- backend/internal/handler/notification/notification.wroker.go
+++ backend/internal/handler/notification/notification.wroker.go
@@ -2,12 +2,15 @@ package notification
 
 import (
 	"context"
+	"fmt"
+	"net/http"
 	"os"
 
+	"github.com/sendinblue/APIv3-go-library/lib"
 	sib_api_v3_sdk "github.com/sendinblue/APIv3-go-library/lib"
 )
 
-func EmailSender(senderName string, senderEmail string, recipientEmail string, subject string, htmlContent string) error {
+func EmailSender(senderName string, senderEmail string, recipientEmail string, subject string, htmlContent string) (lib.CreateSmtpEmail, *http.Response, error) {
 	apiKey := os.Getenv("BREVO_API_KEY")
 
 	cfg := sib_api_v3_sdk.NewConfiguration()
@@ -27,6 +30,8 @@ func EmailSender(senderName string, senderEmail string, recipientEmail string, s
 		HtmlContent: htmlContent,
 	}
 
-	_, _, err := client.TransactionalEmailsApi.SendTransacEmail(context.Background(), email)
-	return err
+	smtRes, httpRes, err := client.TransactionalEmailsApi.SendTransacEmail(context.Background(), email)
+	fmt.Println(err)
+
+	return smtRes, httpRes, err
 }
