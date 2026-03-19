3706841be508061a614215af7760565b182be48b
Teklu <tekluabayneh@gmail.com>
2026-03-19 16:43:15 +0900

    feat: add GetFilePath utility for cross-platform storage support

---
+++ agent/internal/utils/GetFilePath.go
@@ -0,0 +1,50 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+)
+
+func GetStoragePath() (string, string) {
+	var baseDir string
+
+	switch runtime.GOOS {
+	case "windows":
+		// Force Windows to use Program Files or AppData
+		// "C:\Program Files\AgentOrchestrator"
+		exe, _ := os.Executable()
+		baseDir = filepath.Dir(exe)
+
+	case "macOS": // i have to also handerl the macOS one
+		// exe, _ := os.Executable()
+		// baseDir = filepath.Dir(exe)
+
+	case "linux":
+		// Standard Linux path for app data
+		baseDir = "/var/lib/agent-orchestrator"
+	default:
+		// macOS/Unix hidden home folder
+		home, _ := os.UserHomeDir()
+		baseDir = filepath.Join(home, ".agent-orchestrator")
+	}
+
+	finalPath := filepath.Join(baseDir, "internal", "register")
+	RegisterFolder := filepath.Join(baseDir, "register.text")
+
+	// Create it if it's missing
+	err := os.MkdirAll(finalPath, 0755)
+	if err != nil {
+		fmt.Println("err man nega", err)
+		panic(err)
+	}
+
+	_, err = os.Create(RegisterFolder)
+	if err != nil {
+		fmt.Println("err man nega", err)
+		panic(err)
+	}
+
+	return finalPath, RegisterFolder
+}
