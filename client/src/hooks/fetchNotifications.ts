89530661e89422cd40dd4d4dd268856b29f07e67
Teklu <tekluabayneh@gmail.com>
2026-03-26 14:44:55 +0900

    update fetch notifications hook

--- client/src/hooks/fetchNotifications.ts
+++ client/src/hooks/fetchNotifications.ts
@@ -1,11 +1,9 @@
 import toast from "react-hot-toast"
 import api from "../config/axios"
 import { AxiosError } from "axios"
-import { useNavigate } from "react-router-dom"
 
 
-
-export const FechUserNotificationo = async (): UserResponse => {
+export const FechUserNotificationo = async () => {
     try {
         const res = await api.get("/api/get/notificatin")
         return res.data
