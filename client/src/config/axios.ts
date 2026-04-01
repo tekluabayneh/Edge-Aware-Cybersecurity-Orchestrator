69c3dd88c2358dd508ed18a3986eb78ed8b3e1aa
Teklu <tekluabayneh@gmail.com>
2026-04-01 15:25:38 +0900

    fix(client): update axios base url for backend connection

--- client/src/config/axios.ts
+++ client/src/config/axios.ts
@@ -1,18 +1,23 @@
 import axios from "axios"
 
+const BASE_URL = "__VITE_REPLACE_ME__";
 
-const api = axios.create({ baseURL: import.meta.env.VITE_BACKEND_BASE_URL, timeout: 1000, })
+if (!BASE_URL) {
+  console.log("baseURL is not found")
+}
+
+const api = axios.create({ baseURL: BASE_URL, timeout: 20000, })
 
 // add token to each request
 api.interceptors.request.use(async (config) => {
-    const token = await cookieStore.get("token")
-    if (!token) {
-        return
-    }
-    if (token?.value) {
-        config.headers.Authorization = `Bearer ${token.value}`;
-    }
-    return config
+  const token = await cookieStore.get("token")
+  if (!token) {
+    return
+  }
+  if (token?.value) {
+    config.headers.Authorization = `Bearer ${token.value}`;
+  }
+  return config
 })
 
 export default api
